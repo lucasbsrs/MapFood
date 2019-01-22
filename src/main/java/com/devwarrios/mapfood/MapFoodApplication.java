4ac75cfd99b0f4b4e20ac300aee253f6aaeeff76
Lucas Batista da Silva <lucasbsrs@gmail.com>
2019-01-21 22:57:40 -0200

    first commit

---
+++ src/main/java/com/devwarrios/mapfood/MapFoodApplication.java
@@ -0,0 +1,14 @@
+package com.devwarrios.mapfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapFoodApplication.class, args);
+	}
+
+}
+

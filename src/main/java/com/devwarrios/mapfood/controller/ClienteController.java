eb4d588dd429c2ec63f28523aebbe21bd3d08e39
Julian Luís <julian.lfs@gmail.com>
2019-01-25 21:49:55 -0200

    Primeiro controller, ainda bem inicial

---
+++ src/main/java/com/devwarrios/mapfood/controller/ClienteController.java
@@ -0,0 +1,26 @@
+package com.devwarrios.mapfood.controller;
+
+import com.devwarrios.mapfood.model.Cliente;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @RequestMapping(value = "/clientes", method = RequestMethod.GET)
+    public List<Cliente> getClientes(){
+
+        //simula dados obtidos do ClienteRepository
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente(1, 50.0, 60.0));
+        clientes.add(new Cliente(2, 50.0, 60.0));
+
+        return clientes;
+    }
+
+}

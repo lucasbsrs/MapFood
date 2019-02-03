28aacbac7b628912e3ba59d587cd25c72b5ff82c
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2019-02-03 15:11:32 -0200

    Implementa mais um pouco do endpoint de pedidos

--- src/main/java/com/devwarrios/mapfood/controller/PedidoController.java
+++ src/main/java/com/devwarrios/mapfood/controller/PedidoController.java
@@ -1,27 +1,29 @@
 package com.devwarrios.mapfood.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devwarrios.mapfood.dto.PedidoRequestDto;
-import com.devwarrios.mapfood.dto.factory.PedidoFactory;
-import com.devwarrios.mapfood.model.Pedido;
+import com.devwarrios.mapfood.dto.PedidoResponseDto;
+import com.devwarrios.mapfood.service.PedidoService;
 
 @RestController
-@RequestMapping("pedidos")
+@RequestMapping("pedido")
 public class PedidoController {
 
-	/*
-	 * @Autowired PedidoService pedidoService;
-	 */
+	@Autowired
+	PedidoService pedidoService;
 
 	@PostMapping
+	@ResponseBody
 	public ResponseEntity<?> criaPedido(@RequestBody PedidoRequestDto pedidoRequestDto) {
-		Pedido pedido = PedidoFactory.criaPedido(pedidoRequestDto);
+		PedidoResponseDto pedidoResponseDto = pedidoService.criaPedido(pedidoRequestDto);
 
-		return null;
+		return ResponseEntity.ok(pedidoResponseDto);
 	}
 }
--- src/main/java/com/devwarrios/mapfood/dto/factory/PedidoFactory.java
+++ src/main/java/com/devwarrios/mapfood/dto/factory/PedidoFactory.java
@@ -1,52 +1,17 @@
 package com.devwarrios.mapfood.dto.factory;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.devwarrios.mapfood.dto.ItemPedidoDto;
-import com.devwarrios.mapfood.dto.PedidoRequestDto;
 import com.devwarrios.mapfood.model.Cliente;
 import com.devwarrios.mapfood.model.Estabelecimento;
 import com.devwarrios.mapfood.model.ItemPedido;
 import com.devwarrios.mapfood.model.Pedido;
 import com.devwarrios.mapfood.model.PedidoStatus;
-import com.devwarrios.mapfood.model.Produto;
-import com.devwarrios.mapfood.repository.ClienteRepository;
-import com.devwarrios.mapfood.repository.EstabelecimentoRepository;
-import com.devwarrios.mapfood.repository.ProdutoRepository;
 
 public class PedidoFactory {
 
-	@Autowired
-	private static ProdutoRepository produtoRepository;
-
-	@Autowired
-	private static ClienteRepository clienteRepository;
-
-	@Autowired
-	private static EstabelecimentoRepository estabelecimentoRepository;
-
-	public static Pedido criaPedido(PedidoRequestDto pedidoRequestDto) {
-		// Mover boa parte dessa logica para um Service.
-		String clienteId = pedidoRequestDto.getClienteId();
-		String estabelecimentoId = pedidoRequestDto.getEstabelecimentoId();
-
-		Cliente cliente = clienteRepository.findByClienteId(clienteId).get(0);
-		Estabelecimento estabelecimento = estabelecimentoRepository.findByEstabelecimentoId(estabelecimentoId)
-				.get(0);
-
-		List<ItemPedidoDto> itensDto = pedidoRequestDto.getItens();
-
-		List<ItemPedido> itens = new ArrayList<>();
-		for (ItemPedidoDto ipd : itensDto) {
-			Produto produto = produtoRepository.findByProdutoId(ipd.getProdutoId()).get(0);
-
-			itens.add(new ItemPedido(produto, ipd.getQuantidade(), ipd.getObservacao()));
-		}
-
+	public static Pedido criaNovoPedido(Cliente cliente, Estabelecimento estabelecimento, List<ItemPedido> itens) {
 		Pedido pedido = new Pedido(cliente, estabelecimento, itens, LocalDate.now(), PedidoStatus.EM_PREPARO);
 
 		return pedido;

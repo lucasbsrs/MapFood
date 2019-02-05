2e1ca3136e50df3e7b64f12ac9e3018e3d8da1b5
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2019-02-04 22:58:11 -0200

    Atualiza repositórios para serem MongoRepository

--- src/main/java/com/devwarrios/mapfood/repository/ClienteRepository.java
+++ src/main/java/com/devwarrios/mapfood/repository/ClienteRepository.java
@@ -2,12 +2,12 @@ package com.devwarrios.mapfood.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devwarrios.mapfood.model.Cliente;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends MongoRepository<Cliente, Long> {
 	public List<Cliente> findByClienteId(String clienteId);
 }
--- src/main/java/com/devwarrios/mapfood/repository/EstabelecimentoRepository.java
+++ src/main/java/com/devwarrios/mapfood/repository/EstabelecimentoRepository.java
@@ -2,12 +2,12 @@ package com.devwarrios.mapfood.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devwarrios.mapfood.model.Estabelecimento;
 
 @Repository
-public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Long> {
+public interface EstabelecimentoRepository extends MongoRepository<Estabelecimento, Long> {
 	public List<Estabelecimento> findByEstabelecimentoId(String estabelecimentoId);
 }

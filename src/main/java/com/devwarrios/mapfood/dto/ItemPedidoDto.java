36a6a2512657eb991f165a7985b1e97bd0c592d5
Lucas Batista da Silva <lucasbsrs@gmail.com>
2019-02-08 16:07:48 -0200

    Testando Travis CI

--- src/main/java/com/devwarrios/mapfood/dto/ItemPedidoDto.java
+++ src/main/java/com/devwarrios/mapfood/dto/ItemPedidoDto.java
@@ -12,4 +12,34 @@ public class ItemPedidoDto {
 	private String produtoId;
 	private Integer quantidade;
 	private String observacao;
+	
+	public ItemPedidoDto() {
+		
+	}
+
+	public String getProdutoId() {
+		return produtoId;
+	}
+
+	public void setProdutoId(String produtoId) {
+		this.produtoId = produtoId;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+	
+	
 }
--- src/main/java/com/devwarrios/mapfood/dto/PedidoRequestDto.java
+++ src/main/java/com/devwarrios/mapfood/dto/PedidoRequestDto.java
@@ -14,4 +14,34 @@ public class PedidoRequestDto implements PedidoDto {
 	private String clienteId;
 	private String estabelecimentoId;
 	private List<ItemPedidoDto> itens;
+	
+	public PedidoRequestDto() {
+		
+	}
+
+	public String getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(String clienteId) {
+		this.clienteId = clienteId;
+	}
+
+	public String getEstabelecimentoId() {
+		return estabelecimentoId;
+	}
+
+	public void setEstabelecimentoId(String estabelecimentoId) {
+		this.estabelecimentoId = estabelecimentoId;
+	}
+
+	public List<ItemPedidoDto> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemPedidoDto> itens) {
+		this.itens = itens;
+	}
+	
+	
 }

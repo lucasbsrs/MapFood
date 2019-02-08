93b88050458d0c2f06cd72cf401c5b2fa38f2068
Isa Giongo <isagiongo@yahoo.com.br>
2019-02-08 17:39:53 -0200

    Update SwaggerConfig.java

--- src/main/java/com/devwarrios/mapfood/config/SwaggerConfig.java
+++ src/main/java/com/devwarrios/mapfood/config/SwaggerConfig.java
@@ -19,22 +19,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	private final ResponseMessage m201 = simpleMessage (201, "Recurso criado");
-	private final ResponseMessage m204put = simpleMessage (204, "Atualizado com sucesso");
-	private final ResponseMessage m204del = simpleMessage (204, "Excluído com sucesso");
-	private final ResponseMessage m403 = simpleMessage (403, "Não autorizado");
-	private final ResponseMessage m404 = simpleMessage (404, "Não encontrado");
-	private final ResponseMessage m422 = simpleMessage (422, "Erro de validação");
-	private final ResponseMessage m500 = simpleMessage (500, "Erro inesperado");
+// 	private final ResponseMessage m201 = simpleMessage (201, "Recurso criado");
+// 	private final ResponseMessage m204put = simpleMessage (204, "Atualizado com sucesso");
+// 	private final ResponseMessage m204del = simpleMessage (204, "Excluído com sucesso");
+// 	private final ResponseMessage m403 = simpleMessage (403, "Não autorizado");
+// 	private final ResponseMessage m404 = simpleMessage (404, "Não encontrado");
+// 	private final ResponseMessage m422 = simpleMessage (422, "Erro de validação");
+// 	private final ResponseMessage m500 = simpleMessage (500, "Erro inesperado");
 
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.useDefaultResponseMessages(false)
-				.globalResponseMessage(RequestMethod.GET, Arrays.asList(m403, m404, m500))
-				.globalResponseMessage(RequestMethod.POST, Arrays.asList(m201, m403, m422, m500))
-				.globalResponseMessage(RequestMethod.PUT, Arrays.asList(m204put, m403, m404, m422, m500))
-				.globalResponseMessage(RequestMethod.DELETE, Arrays.asList(m204del, m403, m404, m500))
+				.useDefaultResponseMessages(true)
+				//.globalResponseMessage(RequestMethod.GET, Arrays.asList(m403, m404, m500))
+				//.globalResponseMessage(RequestMethod.POST, Arrays.asList(m201, m403, m422, m500))
+				//.globalResponseMessage(RequestMethod.PUT, Arrays.asList(m204put, m403, m404, m422, m500))
+				//.globalResponseMessage(RequestMethod.DELETE, Arrays.asList(m204del, m403, m404, m500))
 				
 				
 				.select()

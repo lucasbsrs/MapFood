6568637a5befc2e360df649e3c41b979f8add836
Fabiano Ramires <fdrtec@gmail.com>
2019-02-11 00:05:57 -0200

    100% cobertura para classe EstabelecimentoController

--- src/test/java/com/devwarriors/mapfood/controller/EstabelecimentoControllerTest.java
+++ src/test/java/com/devwarriors/mapfood/controller/EstabelecimentoControllerTest.java
@@ -1,36 +1,35 @@
 package com.devwarriors.mapfood.controller;
 
 import com.devwarriors.mapfood.model.Estabelecimento;
+import com.devwarriors.mapfood.model.Relatorio;
 import com.devwarriors.mapfood.service.EstabelecimentoService;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Arrays;
+import java.time.LocalDate;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.when;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc
-
 @RunWith(PowerMockRunner.class)
 @PowerMockRunnerDelegate(SpringRunner.class)
 public class EstabelecimentoControllerTest {
 
     private static final String CLIENTE_ID = "2";
-    private static final String EXPECTED_RESPONSE = "<200 OK OK,[com.devwarriors.mapfood.model.Estabelecimento#0 bean],[]>";
+    private static final String RESPONSE_LIST_ESPERADO = "<200 OK OK,[com.devwarriors.mapfood.model.Estabelecimento#0 bean],[]>";
+    private static final String RESPONSE_RELATORIO_ESPERADO = "<200 OK OK,com.devwarriors.mapfood.model.Relatorio#0 bean,[]>";
     private static final Double RAIO = 0.3;
+    private static final String ESTABELECIMENTO_ID = "5640f4538237d9c4aaf3b751c4d11769b3fc1e3165ed3b912c508768f7fc15fd";
+    private static final LocalDate DATA_INICIAL = LocalDate.ofEpochDay(2019-02-9);
+    private static final LocalDate DATA_FINAL = LocalDate.ofEpochDay(2019-02-10);
 
     @InjectMocks
     private EstabelecimentoController controller;
@@ -38,6 +37,8 @@ public class EstabelecimentoControllerTest {
     private EstabelecimentoService service;
     @MockBean
     private Estabelecimento estabelecimento;
+    @MockBean
+    private Relatorio relatorio;
     private List<Estabelecimento> estabelecimentos;
 
     @Before
@@ -47,18 +48,20 @@ public class EstabelecimentoControllerTest {
 
     @Test
     public void geraRelatorio() {
+        when(service.buscaEstabelecimentoPorId(ESTABELECIMENTO_ID)).thenReturn(estabelecimento);
+        when(service.geraRelatorio(ESTABELECIMENTO_ID, DATA_INICIAL, DATA_FINAL)).thenReturn(relatorio);
+        assertEquals(RESPONSE_RELATORIO_ESPERADO, controller.geraRelatorio(ESTABELECIMENTO_ID, DATA_INICIAL, DATA_FINAL).toString());
     }
 
     @Test
     public void metodo_getEstabelecimentos_deve_retornar_reponse_com_uma_lista_de_estabelecimentos() {
         when(service.getEstabelecimentos()).thenReturn(estabelecimentos);
-        assertEquals(EXPECTED_RESPONSE, controller.getEstabelecimentos().toString());
+        assertEquals(RESPONSE_LIST_ESPERADO, controller.getEstabelecimentos().toString());
     }
 
     @Test
-    public void getEstabelecimentosPorLocalizacao() {
+    public void metodo_getEstabelecimentosPorLocalizacaodeve_retornar_reponse_com_uma_lista_de_estabelecimentos() {
         when(service.getEstabelecimentosPorLocalizacao(CLIENTE_ID, RAIO)).thenReturn(estabelecimentos);
-        assertEquals(EXPECTED_RESPONSE, controller.getEstabelecimentosPorLocalizacao(CLIENTE_ID, RAIO).toString());
+        assertEquals(RESPONSE_LIST_ESPERADO, controller.getEstabelecimentosPorLocalizacao(CLIENTE_ID, RAIO).toString());
     }
 }
-

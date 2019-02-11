753851a116c6402b92717d274c767b2d2e2886aa
Fabiano Ramires <fdrtec@gmail.com>
2019-02-10 23:42:10 -0200

    cobertura 66% EstabelecimentoController

--- src/test/java/com/devwarriors/mapfood/controller/EstabelecimentoControllerTest.java
+++ src/test/java/com/devwarriors/mapfood/controller/EstabelecimentoControllerTest.java
@@ -16,31 +16,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Arrays;
 import java.util.List;
 
-@RunWith(PowerMockRunner.class)
-@PowerMockRunnerDelegate(SpringRunner.class)
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@AutoConfigureMockMvc
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(SpringRunner.class)
 public class EstabelecimentoControllerTest {
 
-    private final String EXPECTED_RESPONSE = "<200 OK OK,[com.devwarriors.mapfood.model.Estabelecimento#0 bean],[]>";
+    private static final String CLIENTE_ID = "2";
+    private static final String EXPECTED_RESPONSE = "<200 OK OK,[com.devwarriors.mapfood.model.Estabelecimento#0 bean],[]>";
+    private static final Double RAIO = 0.3;
 
     @InjectMocks
     private EstabelecimentoController controller;
-
     @MockBean
     private EstabelecimentoService service;
-
     @MockBean
     private Estabelecimento estabelecimento;
-
-   private List<Estabelecimento> estabelecimentos;
-
+    private List<Estabelecimento> estabelecimentos;
 
     @Before
     public void setUp() throws Exception {
-        estabelecimentos = Arrays.asList(estabelecimento);
+        estabelecimentos = asList(estabelecimento);
     }
 
     @Test
@@ -49,12 +51,14 @@ public class EstabelecimentoControllerTest {
 
     @Test
     public void metodo_getEstabelecimentos_deve_retornar_reponse_com_uma_lista_de_estabelecimentos() {
-        PowerMockito.when(service.getEstabelecimentos()).thenReturn(estabelecimentos);
-        Assert.assertEquals(EXPECTED_RESPONSE, controller.getEstabelecimentos().toString());
+        when(service.getEstabelecimentos()).thenReturn(estabelecimentos);
+        assertEquals(EXPECTED_RESPONSE, controller.getEstabelecimentos().toString());
     }
 
     @Test
     public void getEstabelecimentosPorLocalizacao() {
+        when(service.getEstabelecimentosPorLocalizacao(CLIENTE_ID, RAIO)).thenReturn(estabelecimentos);
+        assertEquals(EXPECTED_RESPONSE, controller.getEstabelecimentosPorLocalizacao(CLIENTE_ID, RAIO).toString());
     }
 }
 
